8acf3427ab12f0d58eb24e74bb1a2ad542a6b02c
Daniel Łojewski <daniel.lojewski@softwarehut.com>
2020-09-07 12:45:59 +0200

    feat: implemented editing.

--- lib/feature/data/edit/edit_bloc.dart
+++ lib/feature/data/edit/edit_bloc.dart
@@ -1,18 +1,25 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:meta/meta.dart';
+import 'package:secpasscrypt/repository/PasswordRepository.dart';
 
 part 'edit_event.dart';
 part 'edit_state.dart';
 
 class EditBloc extends Bloc<EditEvent, EditState> {
+
+  final _passwordRepository = GetIt.I.get<PasswordRepository>();
+
   EditBloc() : super(EditInitial());
 
   @override
-  Stream<EditState> mapEventToState(
-    EditEvent event,
-  ) async* {
-    // TODO: implement mapEventToState
+  Stream<EditState> mapEventToState(EditEvent event,) async* {
+    if (event is UpdateEntry) {
+      yield UpdatingEntry();
+      await _passwordRepository.updatePassword(event.entry);
+      yield EntryUpdated();
+    }
   }
 }
